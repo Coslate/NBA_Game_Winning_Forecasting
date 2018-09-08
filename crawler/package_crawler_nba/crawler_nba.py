a3dba0830f227f575cf036e1081dc4ba1836147b
Coslate <coslate@media.ee.ntu.edu.tw>
2018-09-08 14:47:48 +0800

    Update crawler_nba package

---
+++ crawler/package_crawler_nba/crawler_nba.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+def GetAllWikiAtricleLinks(url, is_debug):
+    html = urlopen("http://en.wikipedia.org"+url)
+    response = BeautifulSoup(html, 'lxml')
+    ret_all_article_links = []
+    all_div_tags = response.findAll('div', {"id":"bodyContent"})
+
+    for ele_tags in all_div_tags:
+        ret_all_article_links += ele_tags.findAll('a', href=re.compile("^(\/wiki\/)((?!:).)*$"))
+
+    if(is_debug):
+        for ele in ret_all_article_links:
+            print("------------------")
+            print(f"ele = {ele}")
+            print(f"attrs = {ele.attrs}")
+            print(f"attrs[href] = {ele.attrs['href']}")
+            print("------------------")
+
+    return ret_all_article_links
+
+
