8e49d5d078855a26fdc30d1764369a617da42182
Coslate <coslate@media.ee.ntu.edu.tw>
2018-09-11 00:39:06 +0800

    Update Web API-ip_stack application & revise the GetAllURLLinks in the package_crawler_nba

--- crawler/package_crawler_nba/crawler_nba.py
+++ crawler/package_crawler_nba/crawler_nba.py
@@ -1,8 +1,11 @@
 from urllib.request import urlopen
+from urllib import error
 from bs4 import BeautifulSoup
 import re
+import sys
+import json
 
-def GetAllWikiAtricleLinks(url, is_debug):
+def GetAllWikiAtricleLinks(url, is_debug=0):
     html = urlopen("http://en.wikipedia.org"+url)
     response = BeautifulSoup(html, 'lxml')
     ret_all_article_links = []
@@ -21,7 +24,7 @@ def GetAllWikiAtricleLinks(url, is_debug):
 
     return ret_all_article_links
 
-def GetAllURLLinks(url, is_debug, pages_links, recursive_num):
+def GetAllURLLinks(url, pages_links, recursive_num, is_debug=0):
     recursive_num += 1
     html = urlopen("http://en.wikipedia.org"+url)
     response = BeautifulSoup(html, 'lxml')
@@ -32,6 +35,7 @@ def GetAllURLLinks(url, is_debug, pages_links, recursive_num):
     print(f'original page_links = {pages_links}')
     print(f'recursive_num = {recursive_num}')
     print('----------------------')
+
     for ele_tags in all_div_tags:
         if(ele_tags.attrs['href'] not in pages_links):
             new_page_link = ele_tags.attrs['href']
@@ -39,5 +43,44 @@ def GetAllURLLinks(url, is_debug, pages_links, recursive_num):
             print(f'new_page_link = {new_page_link}')
             print('----------------------')
             pages_links.add(new_page_link)
-            GetAllURLLinks(new_page_link, 1, pages_links, recursive_num)
+            try:
+                (recursive_num, page_links) = GetAllURLLinks(new_page_link, pages_links, recursive_num, 1)
+            except RecursionError:
+                print(f"> maximum recursion depth exceeded.")
+                print(f"> recursive_num = {recursive_num}")
+                print(f"> Program Terminated.")
+                return (recursive_num, page_links)
 
+    return (recursive_num, page_links)
+
+def GetEditHistoryIPList(url, is_debug=0):
+    # The format of the URL of an editing-history-page is as the following:
+    # http://en.wikipedia.org/w/index.php?title=<title_in_url>&action=history
+    url = url.replace('/wiki/', '')
+    history_url = 'http://en.wikipedia.org/w/index.php?title='+url+'&action=history'
+    if(is_debug):
+        print(f'history_url = {history_url}')
+
+    html = urlopen(history_url)
+    response_obj = BeautifulSoup(html, 'lxml')
+    all_ip_possible_address = response_obj.findAll('a', {'class':'mw-userlink mw-anonuserlink'})
+    address_list = set()
+    for ip_address in all_ip_possible_address:
+        ip_address_txt = ip_address.get_text()
+        address_list.add(ip_address_txt)
+
+        if(is_debug):
+            print('---------------------------')
+            print(f"ip_address = {ip_address}")
+            print(f"ip_address_txt = {ip_address_txt}")
+
+    return address_list
+
+def GetCountry(ip_address):
+    access_key = "7d1d13cee5f609b669d5777029ec0e4f"
+    try:
+        response = urlopen('http://api.ipstack.com/'+ip_address+'?access_key='+access_key).read().decode('utf-8')
+    except error.HTTPError:
+        return None
+    response_json = json.loads(response)
+    return response_json.get('country_name')
--- crawler/test_traverse_all_links_wiki.py
+++ crawler/test_traverse_all_links_wiki.py
@@ -6,9 +6,17 @@ import re
 import package_crawler_nba.crawler_nba as crawler_nba
 import random
 import datetime
+import sys
 
 print(f"time = {datetime.datetime.now()}")
+sys.setrecursionlimit(1500)
+print(f'python recursion limit = {sys.getrecursionlimit()}')
 
 pages_links = set()
 recursive_num = 0
-crawler_nba.GetAllURLLinks("", 1, pages_links, recursive_num)
+(recursive_num, pages_links) = crawler_nba.GetAllURLLinks("", pages_links, recursive_num, 1)
+
+print('--Final--')
+print(f'recursive_num = {recursive_num}')
+for pages in pages_links:
+    print(f'pages = {pages}')
