dd53e86e4676d1c105e127ada51e60ca5964b392
Coslate <coslate@media.ee.ntu.edu.tw>
2018-09-15 23:21:36 +0800

    Update method of getting image - urlretrieve

---
+++ database/test_get_img.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3.6
+
+from urllib.request import urlretrieve
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+#-----------------Execution------------------#
+if __name__ == '__main__':
+    html = urlopen("http://www.pythonscraping.com")
+    bs_obj = BeautifulSoup(html, 'lxml')
+    all_image_locs = bs_obj.findAll('a', {"id":"logo"})
+    count = 0
+
+    for image_loc_cand in all_image_locs:
+        image_loc = image_loc_cand.find('img')['src']
+        urlretrieve(image_loc, 'logo_'+str(count)+'.jpg')
+        count += 1
