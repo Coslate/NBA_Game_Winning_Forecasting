ca5192acfedeace8c0d6a5dd27ff38f38f70c644
Coslate <coslate@media.ee.ntu.edu.tw>
2018-09-19 23:49:23 +0800

    Update the test proxy code

---
+++ crawler/test_proxy.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3.6
+
+from urllib.error import URLError
+from urllib.request import ProxyHandler, build_opener
+
+#proxy = 'username:password@127.0.0.1:9743'
+proxy = '117.241.96.218:58192'
+proxy_handler = ProxyHandler({
+    'http': 'http://' + proxy,
+    'https': 'https://' + proxy
+})
+opener = build_opener(proxy_handler)
+try:
+    response = opener.open('http://httpbin.org/get')
+    print(response.read().decode('utf-8'))
+except URLError as e:
+    print(e.reason)
