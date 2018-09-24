24cda55f9a025b5262dbb7fb68c61762b5b130cb
Coslate <coslate@media.ee.ntu.edu.tw>
2018-09-25 00:34:05 +0800

    Update getting text editors table from wiki

--- database/test_get_tables_to_csv.py
+++ database/test_get_tables_to_csv.py
@@ -6,15 +6,21 @@ from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
     html = urlopen("https://en.wikipedia.org/wiki/Comparison_of_text_editors")
-    bs_obj = BeautifulSoup(html)
-    print(bs_obj.prettify())
-    csv_file = open('./files/table.csv', 'w+', newline='', encoding='utf-8')
-'''
+    bs_obj = BeautifulSoup(html, 'lxml')
+
+    #Get the first table of the page
+    table = bs_obj.findAll('table', {'class':'wikitable'})[0]
+    rows = table.findAll('tr')
+
+    #Open the csv_file
+    csv_file = open('./files/table_editors.csv', 'w+', newline='', encoding='utf-8')
+    writer = csv.writer(csv_file)
+
     try:
-        writer = csv.writer(csv_file)
-        writer.writerow(('number', 'number + 2', 'number * 2'))
-        for i in range(10):
-            writer.writerow((i, i+2, i*2))
+        for row in rows:
+            csv_row = []
+            for cell in row.findAll(['td', 'th']):
+                csv_row.append(cell.get_text())
+            writer.writerow(csv_row)
     finally:
         csv_file.close()
-'''
