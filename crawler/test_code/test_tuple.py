44e51cf6b7ec35fa29a07fd6bab9bb3df87007de
Coslate <coslate@media.ee.ntu.edu.tw>
2019-02-17 23:55:24 +0800

    Add test code

---
+++ crawler/test_code/test_tuple.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3.6
+
+first_names = ['Fred', 'George', 'Bill']
+last_names = ['Smith', 'Jones', 'Williams']
+name_tuple = (first_names, last_names)
+
+first_names.append('Igor')
+
+print(f'first_names = {first_names}')
+print(f'name_tuple = {name_tuple}')
